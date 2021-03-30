5dc32a6e8405e2b3936be125bf2918b7610577e3
Mudassir Iqbal <m.mudassir@magicmindtechnologies.com>
2021-03-30 14:27:42 +0530

    expose all function in index

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const db = require('./lib/database/db');
 
-const cart = exports;
 const {
 	addCart,
 	getCart,
@@ -10,12 +9,32 @@ const {
 	removeCondition,
 } = require('./lib/controllers/cart.controller');
 
-cart.addCart = async (content, globalConditions, userId) => {
+exports.addCart = async (content, globalConditions, userId) => {
 	await db.sequelize.sync();
 	return addCart(content, globalConditions, userId);
 };
 
-cart.getCart = async userId => {
+exports.getCart = async userId => {
 	await db.sequelize.sync();
 	return getCart(userId);
+};
+
+exports.updateCart = async (content, globalConditions, userId) => {
+	await db.sequelize.sync();
+	return updateCart(content, globalConditions, userId);
+};
+
+exports.deleteCart = async userId => {
+	await db.sequelize.sync();
+	return deleteCart(userId);
+};
+
+exports.updateItemQuantity = async (userId, _itemId, quantity) => {
+	await db.sequelize.sync();
+	return updateItemQuantity(userId, _itemId, quantity);
+};
+
+exports.removeCondition = async (userId, _itemId, _conditionId) => {
+	await db.sequelize.sync();
+	return removeCondition(userId, _itemId, _conditionId);
 };
